4fe0472906555161118c3d9b8cec515c019e7d5e
Sernikjamnika <mati.walczak997@gmail.com>
2017-10-28 00:19:31 +0200

    Add coinhive router

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var pagestats = require('./routes/pagestats');
+var pagestats = require('./routes/userstats');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/pagestats', pagestats);
+app.use('/userstats', userstats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Server/routes/userstats.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var secret_key = '8wrE1OOuytBY2nogXsQXwLqji5atfEJd';
+
+router.get('/', function(req, res, next){
+    var request = require('request');
+    
+    // Set the headers
+    var headers = {
+        'Content-Type':     'application/json'
+    }
+    
+    // Start the request
+    request("https://api.coinhive.com/stats/site?secret=8wrE1OOuytBY2nogXsQXwLqji5atfEJd", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            parsedBody = JSON.parse(body);
+            console.log(parsedBody["hashesTotal"]);
+            console.log(Number(parsedBody["xmrPending"].toString()).toPrecision(4));
+            res.render('pagestats');
+        }
+    })
+});
+
+module.exports = router;
