63b273ca5f5fe8e7c2eac34147b5ed88d7fb7028
Maciej Hajduk <ebrithil97@gmail.com>
2017-10-27 22:29:44 +0200

    reapir try nO1

--- MOBILE/screens/HomeScreen.js
+++ MOBILE/screens/HomeScreen.js
@@ -1,140 +1,175 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Button,
-  TextInput
-} from 'react-native';
-import {WebBrowser} from 'expo';
-import RootNavigation from '../navigation/RootNavigation';
-import {StackNavigator} from 'react-navigation';
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Image, Text, View, WebView } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import { WebBrowser } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import Touchable from 'react-native-platform-touchable';
 
-export default class HomeScreen extends React.Component {
+
+
+export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    header: null
+    title: 'Watch ads', header: null
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: ""
-    }
-  }
-
   render() {
-    const {navigate} = this.props.navigation;
     return (
-
-      <View style={styles.container}>
-        <Image style={styles.img} source={require('../assets/images/logo.png')}/>
-
-        <View style={styles.flexBox1}>
-
-          <TextInput
-            style={styles.form}
-            placeholder="username"
-            onChangeText={(username) => this.setState({username})}
-            value={this.state.username}/>
-          <TextInput/>
-
-          <TextInput
-            style={styles.form}
-            secureTextEntry={true}
-            placeholder="password"
-            onChangeText={(password) => this.setState({password})}
-            value={this.state.password}/>
-          <TextInput/>
-
-          <TouchableOpacity style={styles.loginTouch} onPress={() => navigate('App')}>
-            <Text style={styles.loginButton}>
-              Login
-            </Text>
-
-          </TouchableOpacity>
-
+      <ScrollView style={styles.container}>
+        <Touchable
+        style={styles.option}
+        background={Touchable.Ripple('#ccc', false)}
+        onPress={this._handleAddWatch}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Image
+              source={require('../assets/images/add2.png')}
+              resizeMode="contain"
+              fadeDuration={0}
+              style={{ width: 96, height: 64, marginTop: 1 }}
+            />
+          </View>
+          <View style={styles.optionTextContainer}>
+            <Text style={styles.optionText}>
+              Watch this advertisement!
+          </Text>
+          </View>
         </View>
+      </Touchable>
 
-      </View>
+        <Touchable
+        style={styles.option}
+        background={Touchable.Ripple('#ccc', false)}
+        onPress={this._handleAddWatch}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Image
+              source={require('../assets/images/add1.png')}
+              resizeMode="contain"
+              fadeDuration={0}
+              style={{ width: 96, height: 64, marginTop: 1 }}
+            />
+          </View>
+          <View style={styles.optionTextContainer}>
+            <Text style={styles.optionText}>
+            Watch this advertisement!
+          </Text>
+          </View>
+        </View>
+      </Touchable>
+
+        <Touchable
+        style={styles.option}
+        background={Touchable.Ripple('#ccc', false)}
+        onPress={this._handleAddWatch}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Image
+              source={require('../assets/images/add2.png')}
+              resizeMode="contain"
+              fadeDuration={0}
+              style={{ width: 96, height: 64, marginTop: 1 }}
+            />
+          </View>
+          <View style={styles.optionTextContainer}>
+            <Text style={styles.optionText}>
+            Watch this advertisement!
+          </Text>
+          </View>
+        </View>
+      </Touchable>
+
+        <Touchable
+        style={styles.option}
+        background={Touchable.Ripple('#ccc', false)}
+        onPress={this._handleAddWatch}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Image
+              source={require('../assets/images/add1.png')}
+              resizeMode="contain"
+              fadeDuration={0}
+              style={{ width: 96, height: 64, marginTop: 1 }}
+            />
+          </View>
+          <View style={styles.optionTextContainer}>
+            <Text style={styles.optionText}>
+            Watch this advertisement!
+          </Text>
+          </View>
+        </View>
+      </Touchable>
+
+        <Touchable
+          style={styles.option}
+          background={Touchable.Ripple('#ccc', false)}
+          onPress={this._handleAddWatch}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.optionIconContainer}>
+              <Image
+                source={require('../assets/images/add2.png')}
+                resizeMode="contain"
+                fadeDuration={0}
+                style={{ width: 96, height: 64, marginTop: 1 }}
+              />
+            </View>
+            <View style={styles.optionTextContainer}>
+              <Text style={styles.optionText}>
+              Watch this advertisement!
+            </Text>
+            </View>
+          </View>
+        </Touchable>
+
+        <Touchable
+        style={styles.option}
+        background={Touchable.Ripple('#ccc', false)}
+        onPress={this._handleAddWatch}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.optionIconContainer}>
+            <Image
+              source={require('../assets/images/add1.png')}
+              resizeMode="contain"
+              fadeDuration={0}
+              style={{ width: 96, height: 64, marginTop: 1 }}
+            />
+          </View>
+          <View style={styles.optionTextContainer}>
+            <Text style={styles.optionText}>
+            Watch this advertisement!
+          </Text>
+          </View>
+        </View>
+      </Touchable>
+
+      </ScrollView>
     );
   }
+  _handleAddWatch = () => {
+    WebBrowser.openBrowserAsync('https://i1.jbzdy.pl/contents/2017/10/7126edd068d04f3b7077849dec6cb3dc.mp4');
+  };
 }
-
-login = () => {
-  alert(this.state.username + "   " + this.state.password);
-  navigate('App');
-  // var data = {   "username": 'Maku',   "password": 'daasd' }
-  // fetch('http://207.154.221.96:8080/', {   method: 'POST',   headers: {
-  // 'Accept': '/',     'Accept-Encoding': 'gzip, deflate',     'Content-Length':
-  // '21',     'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent':
-  // 'runscope/0.1'   },     body: JSON.stringify(data)   }) .then(response =>
-  // response.json())   .then(response => { alert(JSON.stringify(response));   })
-  //  .done();
-
-}
-
-/* <View>
-<Button
-onPress={() =>
-navigate('App')
-}
-title="Login"
-color="#841584"
-accessibilityLabel="Learn more about this purple button"
-/> */
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 2,
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'rgb(43, 72, 117)'
+    paddingTop: 3,
+    backgroundColor: '#fff',
   },
-  flexBox1: {
-    flex: 0.65,
-    marginTop: '40%',
-    width: "95%",
-    flexDirection: "column",
-    alignItems: 'center',
-    justifyContent: 'center'
+  optionIconContainer: {
+    marginRight: 9,
   },
-  form: {
-    height: '35%',
-    width: '100%',
-    padding: '0.5%',
-    textAlign: 'center',
-    color: 'rgba(232, 238, 250, 0.7)',
-    backgroundColor: 'rgba(232, 238, 247, 0.4)'
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 3,
+    paddingVertical: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#EDEDED',
   },
-  img: {
-    flexDirection: "column",
-    height: 200,
-    borderRadius: 100,
-    width: 200,
-    marginTop: '25%',
-    borderWidth: 1,
-    borderColor: '#fff',
-    overflow: 'hidden',
-    backgroundColor: '#666',
-    alignItems: 'center',
-    justifyContent: 'center'
+  optionText: {
+    paddingHorizontal: 50,
+    paddingVertical: 23,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#FFFFFF',
   },
-
-  loginTouch: {
-    width: '100%',
-    height: '35%',
-    padding: '1%',
-    backgroundColor: 'rgb(45, 88, 155)'
-  },
-  loginButton: {
-    marginTop: 10,
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#eee',
-    fontWeight: 'bold'
-  }
 });
