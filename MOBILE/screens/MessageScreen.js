86524da1a911f3b75c46791e52a323c06f264c99
Maciej Hajduk <ebrithil97@gmail.com>
2017-10-28 03:37:14 +0200

    added messages

--- MOBILE/screens/MessageScreen.js
+++ MOBILE/screens/MessageScreen.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { GiftedChat } from 'react-native-gifted-chat';
+
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = { title: 'Messages', header: null };
+  state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    );
   }
+
 }
