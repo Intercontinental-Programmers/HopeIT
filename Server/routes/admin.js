80b86d45f7badae50c0834ccf67a46c503c2e0ae
Michał Treter <adimus13@gmail.com>
2017-10-28 10:05:34 +0200

    Sending messages to all works

--- Server/routes/admin.js
+++ Server/routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../controllers/AdminAuthController');
 var User = require('../models/user');
+var Message = require('../models/message.js')
 
 //route to home page
 router.get('/', auth.home);
@@ -33,6 +34,45 @@ router.get('/users_list', function(req, res){
       });
 });
 
+router.get('/messageshistory', function(request, response){
+    
+        Message.find({}, function(err, messages){
+            var messagesList = [];
+            messages.forEach(function(message){
+                messagesList.push(message);
+            });
+            response.render('../views/admin-list-message.ejs', {'messages': messagesList});
+        });
+    });
+
+router.post('/sendall', function(request, response){
+    User.find({}, function(err, users) {
+        users.forEach(function(user) {
+            var message = new Message({
+                email: user.username,
+                topic: request.body.topic,
+                body: request.body.body,
+                read: false, 
+                //image: { data: Buffer, contentType: String }
+            });
+            message.save();
+        });
+        response.render('../views/admin-index.ejs', {admin : request.user});
+    });
+});
+router.get('/sendall',function(request, response){
+    response.render('admin-sendall.ejs');
+});
+
+router.post('/sendselect', function(request, respond){
+    var message = new Message({
+        username: String,
+        firstname: String,
+        surname : String,
+        password: String,
+    });
+})
+
 router.get('/send_one_message', function(req, res){
     User.find({}, function(err, users) {
         var userList = [];
@@ -46,7 +86,7 @@ router.get('/send_one_message', function(req, res){
 });
 
 router.post('/send-one-message', function(req, res){
-    
+
 });
 
 
