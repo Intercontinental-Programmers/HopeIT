e1721f9e64776ddb27c7746fa84784f0389b31bd
Adrian Mucha <kestarumper@users.noreply.github.com>
2017-10-28 02:19:31 +0200

    Revert "Mw coinhive"

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var paypal = require('./routes/paypal')
 
-
 var app = express();
 
 // view engine setup
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/paypal', paypal);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
