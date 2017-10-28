0eec4798c3dde21872f5316b2cb4047bd26651ee
Michał Treter <adimus13@gmail.com>
2017-10-28 02:48:52 +0200

    Admin login works, so it's nice

--- Server/app.js
+++ Server/app.js
@@ -4,16 +4,44 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+var Admin = require('./models/admin');
 var index = require('./routes/index');
 var users = require('./routes/users');
-var pagestats = require('./routes/pagestats');
-var userstats = require('./routes/userstats');
-var coinhive = require('./routes/coinhive')
+//var pagestats = require('./routes/pagestats');
+//var userstats = require('./routes/userstats');
+var coinhive = require('./routes/coinhive');
+var admin = require('./routes/admin');
+
+
 
 var paypal = require('./routes/paypal')
 
 var app = express();
+
+//database setup
+mongoose.connect('mongodb://test:test123@ds237445.mlab.com:37445/hopeit-icp')
+.then(() =>  console.log('connection succesful'))
+.catch((err) => console.error(err));
+
+//initialize some security shit, man 
+var LocalStrategy = require('passport-local').Strategy;
+app.use(require('express-session')({
+secret: 'prof jacek cichon drugi pali dobre szlugi',
+resave: false,
+saveUninitialized: false
+}));
+
+//passport strategy initialization
+passport.use(new LocalStrategy(Admin.authenticate()));
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,10 +57,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/pagestats', pagestats);
-app.use('/userstats', userstats);
+//app.use('/pagestats', pagestats);
+//app.use('/userstats', userstats);
 app.use('/coinhive', coinhive);
 app.use('/paypal', paypal);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Server/controllers/AdminAuthController.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var passport = require('passport');
+var Admin = require('../models/admin.js');
+
+var adminController = {};
+
+adminController.home = function(req, res) {
+    if(req.user)
+        res.render('../views/admin-index.ejs', {admin : req.user});
+    res.send('Trzeba się zalogować');
+};
+
+adminController.register = function(req, res) {
+    res.render('../views/admin-register.ejs');
+};
+
+adminController.doRegister = function(req, res) {
+    Admin.register(new Admin({username : req.body.email, firstname : req.body.firstname,
+                                surname : req.body.surname}), req.body.password,
+                                function(err, admin) {
+                                    if(err){
+                                        console.log(err);
+                                        return res.render('../views/admin-register.ejs', { admin : req.user });
+                                    }
+                                    passport.authenticate('local')(req, res, function(){
+                                        console.log('dobrze');
+                                        res.redirect('/');
+                                    })
+                                });
+};
+
+adminController.login = function(req, res){
+    res.render('../views/admin-login.ejs');
+};
+
+adminController.doLogin = function(req, res) {
+    passport.authenticate('local')(req, res, function () {
+        console.log('Zalogowano');
+      res.redirect('/admin');
+    });
+  };
+
+adminController.logout = function(req, res) {
+    req.logout();
+    res.redirect('/admin');
+  };
+
+module.exports = adminController;
