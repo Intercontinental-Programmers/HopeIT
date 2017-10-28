2fc72e1904fbfe39f0308fa7ae427844d9791dc5
Maciej Hajduk <ebrithil97@gmail.com>
2017-10-28 11:49:35 +0200

    workin donation

--- MOBILE/screens/DonateScreen.js
+++ MOBILE/screens/DonateScreen.js
@@ -7,160 +7,138 @@ import {
     TouchableHighlight,
     Button,
     Image,
-    StyleSheet,
+    StyleSheet
 } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { WebBrowser } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import Touchable from 'react-native-platform-touchable';
 
+var variable = 'null';
 
 export default class DonateScreen extends React.Component {
     static navigationOptions = {
-        title: 'Donate', header: null
+        title: 'Donate',
+        header: null
     };
 
     state = {
-        modalVisible: false,
+        modalVisible: false
     }
 
     setModalVisible(visible) {
         this.setState({ modalVisible: visible });
     }
 
+
+
     render() {
         return (
-            <View style={{ marginTop: 22 }}>
+            <View style={{
+                marginTop: 22
+            }}>
                 <View>
                     <Modal
                         animationType="slide"
                         transparent={false}
                         visible={this.state.modalVisible}
-                        onRequestClose={() => { !this.setModalVisible(false) }}
-                    >
+                        onRequestClose={() => {!this.setModalVisible(false)}}>
+                        
                         <WebView
-                            source={{ uri: 'https://ssl.dotpay.pl/test_payment/?id=726300&kwota=10&opis=Test' }}
-                            style={{ marginTop: 2 }}
-                        />
+                            source={{
+                                uri: variable
+                            }}
+                            style={{
+                                marginTop: 2
+                            }} />
 
-                        <Button style={styles.comeback}
-                            onPress={() => { !this.setModalVisible(false) }}
+                        <Button
+                            style={styles.comeback}
+                            onPress={() => {
+                                !this.setModalVisible(false)
+                            }}
                             title="CLOSE"
                             color="#BBBBBB"
-                            accessibilityLabel="Learn more about this purple button"
-                        />
+                            accessibilityLabel="Learn more about this purple button" />
+                        
                     </Modal>
 
-                    <TouchableHighlight onPress={() => { this.setModalVisible(true) }}
+                    <TouchableHighlight
+                        onPress={() => {
+                            variable = 'https://ssl.dotpay.pl/test_payment/?id=726300&kwota=10&opis=Test';
+                            this.setModalVisible(true);
+                        }}
                         style={styles.button}
                         underlayColor='#2b4875'>
                         <Image
                             source={require('../assets/images/dotpay.png')}
                             resizeMode="contain"
                             fadeDuration={0}
-                            style={{ width: 96, height: 69, marginTop: 1 }}
-                        />
+                            style={{
+                                width: 96,
+                                height: 69,
+                                marginTop: 1
+                            }} />
 
-                    </TouchableHighlight></View>
-                <View style={{ marginTop: 15 }}>
+                    </TouchableHighlight>
 
-                    <Modal
-                        animationType="slide"
-                        transparent={false}
-                        visible={this.state.modalVisible}
-                        onRequestClose={() => { !this.setModalVisible(false) }}
-                    >
-                        <WebView
-                            source={{ uri: 'https://ssl.dotpay.pl/test_payment/?id=726300&kwota=10&opis=Test' }}
-                            style={{ marginTop: 2 }}
-                        />
-
-                        <Button style={styles.comeback}
-                            onPress={() => { !this.setModalVisible(false) }}
-                            title="CLOSE"
-                            color="#BBBBBB"
-                            accessibilityLabel="Learn more about this purple button"
-                        />
-                    </Modal>
-
-                    <TouchableHighlight onPress={() => { this.setModalVisible(true) }}
-                        style={styles.button}
-                        underlayColor='#2b4875'>
-                        <Image
-                            source={require('../assets/images/paypal.png')}
-                            resizeMode="contain"
-                            fadeDuration={0}
-                            style={{ width: 96, height: 69, marginTop: 1 }}
-                        />
-
-                    </TouchableHighlight></View>
-
-                    <View style={{ marginTop: 15 }}>
-
-                    <Modal
-                        animationType="slide"
-                        transparent={false}
-                        visible={this.state.modalVisible}
-                        onRequestClose={() => { !this.setModalVisible(false) }}
-                    >
-                        <WebView
-                            source={{ uri: 'https://ssl.dotpay.pl/test_payment/?id=726300&kwota=10&opis=Test' }}
-                            style={{ marginTop: 2 }}
-                        />
-
-                        <Button style={styles.comeback}
-                            onPress={() => { !this.setModalVisible(false) }}
-                            title="CLOSE"
-                            color="#BBBBBB"
-                            accessibilityLabel="Learn more about this purple button"
-                        />
-                    </Modal>
-
-                    <TouchableHighlight onPress={() => { this.setModalVisible(true) }}
-                        style={styles.button}
-                        underlayColor='#2b4875'>
-                        <Image
-                            source={require('../assets/images/przelewy24.png')}
-                            resizeMode="contain"
-                            fadeDuration={0}
-                            style={{ width: 96, height: 69, marginTop: 1 }}
-                        />
-
-                    </TouchableHighlight></View>
-
-                                    <View style={{ marginTop: 15 }}>
-
-                    <Modal
-                        animationType="slide"
-                        transparent={false}
-                        visible={this.state.modalVisible}
-                        onRequestClose={() => { !this.setModalVisible(false) }}
-                    >
-                        <WebView
-                            source={{ uri: 'https://ssl.dotpay.pl/test_payment/?id=726300&kwota=10&opis=Test' }}
-                            style={{ marginTop: 2 }}
-                        />
-
-                        <Button style={styles.comeback}
-                            onPress={() => { !this.setModalVisible(false) }}
-                            title="CLOSE"
-                            color="#BBBBBB"
-                            accessibilityLabel="Learn more about this purple button"
-                        />
-                    </Modal>
-
-                    <TouchableHighlight onPress={() => { this.setModalVisible(true) }}
+                    <TouchableHighlight
+                        onPress={() => {
+                            variable = 'http://developers.payu.com/pl/';
+                            this.setModalVisible(true);
+                        }}
                         style={styles.button}
                         underlayColor='#2b4875'>
                         <Image
                             source={require('../assets/images/payu.png')}
                             resizeMode="contain"
                             fadeDuration={0}
-                            style={{ width: 96, height: 69, marginTop: 1 }}
-                        />
+                            style={{
+                                width: 96,
+                                height: 69,
+                                marginTop: 1
+                            }} />
 
-                    </TouchableHighlight></View>
+                    </TouchableHighlight>
 
+                    <TouchableHighlight
+                        onPress={() => {
+                            variable = 'https://www.przelewy24.pl/';
+                            this.setModalVisible(true);
+                        }}
+                        style={styles.button}
+                        underlayColor='#2b4875'>
+                        <Image
+                            source={require('../assets/images/przelewy24.png')}
+                            resizeMode="contain"
+                            fadeDuration={0}
+                            style={{
+                                width: 96,
+                                height: 69,
+                                marginTop: 1
+                            }} />
+
+                    </TouchableHighlight>
+
+                    <TouchableHighlight
+                        onPress={() => {
+                            variable = 'http://207.154.221.96:3000/paypal/69/PLN';
+                            this.setModalVisible(true);
+                        }}
+                        style={styles.button}
+                        underlayColor='#2b4875'>
+                        <Image
+                            source={require('../assets/images/paypal.png')}
+                            resizeMode="contain"
+                            fadeDuration={0}
+                            style={{
+                                width: 96,
+                                height: 69,
+                                marginTop: 1
+                            }} />
+
+                    </TouchableHighlight>
+                </View>
             </View>
         );
     }
@@ -168,9 +146,9 @@ export default class DonateScreen extends React.Component {
 
 const styles = StyleSheet.create({
     button: {
-        alignItems: 'center',
+        alignItems: 'center'
     },
     comeback: {
-        height: 30,
-    },
+        height: 30
+    }
 });
